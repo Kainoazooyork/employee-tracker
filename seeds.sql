07a85752253363223f6763e2810fc78c43ae43db
Kainoa Furtado <144808230+Kainoazooyork@users.noreply.github.com>
2024-03-19 15:12:07 -0700

    fixed syntax errors

--- seeds.sql
+++ seeds.sql
@@ -1,19 +1,19 @@
 INSERT INTO department (name)
 VALUES  ("Top Executive"),
-        ("Front Office");
+        ("Front Office"),
         ("Field General"),
         ("Coaching Department"),
         ("Field Committee");
     
 INSERT INTO role (title, salary, department_id)
-VALUES  ("President of Football Operations", 10,000,000 , 1),
-        ("General Manager", 8,500,000, 2),
-        ("Head Coach", 14,000,000, 3),
-        ("Assistant Coach", 2,500,000, 4),
-        ("Ball Boy", 12,000, 5);
+VALUES  ("President of Football Operations", 1000000, 1),
+        ("General Manager", 8500000, 2),
+        ("Head Coach", 14000000, 3),
+        ("Assistant Coach", 2500000, 4),
+        ("Ball Boy", 12000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES  ("Chuck", "Armstrong", 1, 2),
+VALUES  ("Chuck", "Armstrong", 1, NULL),
         ("John", "Schneider", 2, NULL),
         ("Mike", "MacDonald", 3, 2),
         ("Leslie", "Frazier", 4, NULL),
