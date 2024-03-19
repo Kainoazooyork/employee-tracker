3d66b277331b465eafa3fb70905a4e44920df893
Kainoa Furtado <144808230+Kainoazooyork@users.noreply.github.com>
2024-03-19 15:11:41 -0700

    fixed errors

--- schema.sql
+++ schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE department (
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
+    title VARCHAR(100) NOT NULL,
     salary DECIMAL,
     department_id INT,
     FOREIGN KEY (department_id)
@@ -20,8 +20,8 @@ CREATE TABLE role (
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id)
